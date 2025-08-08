2b35690ffea17dadc6c15890972fec131d77e588
ebin6 <ebin.oneteam@gmail.com>
2025-08-08 19:56:24 +0530

    conditional statements introduction

---
+++ conditional_statements_intro.py
@@ -0,0 +1,26 @@
+"""
+Syntax
+
+if condition:
+   statements
+
+"""
+"""
+age=int(input("Enter your age : "))
+if age>=18:
+    print("Eligible for voting")
+else:
+    print("Not eligible for voting")
+"""
+
+mark=int(input("Enter your mark : "))
+if mark>=95:
+    print("Excellent")
+    print("You secured A+")
+elif mark>=75:
+    print("Good Job")
+    print("You secured B grade")
+elif mark>=50:
+    print("You secured c grade")
+else:
+    print("Keep practicing")
